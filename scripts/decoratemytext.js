46c9b0f579c79fd1c5404b4b5ef4780f7086ece4
Tam Nguyen <tamnguyen@Tams-MacBook-Pro.local>
2020-10-04 10:46:27 -0500

    Assignment 6- additional

--- scripts/decoratemytext.js
+++ scripts/decoratemytext.js
@@ -2,7 +2,7 @@ let timer = null;
 window.onload = function () {
   document.getElementById("decorBtn").onclick = function () {
     if (timer === null) {
-      timer = setInterval(decorateText, 500);
+      timer = setInterval(increaseFontSize, 500);
     } else {
       clearInterval(timer);
       timer = null;
@@ -13,19 +13,30 @@ window.onload = function () {
 
 function bling() {
   let text = document.getElementById("txtarea");
+  let body = document.body;
   let isChecked = document.getElementById("blingId");
   if (isChecked.checked) {
     text.className = "bling";
+    body.className = "backgroundImg";
   } else {
     text.className = "";
+    body.className = "";
   }
 }
 
-function biggerText() {
+function increaseFontSize() {
     var text = document.getElementById('txtarea');
     if(text.style.fontSize && parseInt(text.style.fontSize) < 30)
         text.style.fontSize = (parseInt(text.style.fontSize) + 2) + "pt";
     else {
         text.style.fontSize = "12pt";
     }
+}
+
+function igpayAtinlayExchange() {
+
+}
+
+function malkovitchEffect() {
+    
 }
